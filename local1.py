184ec3358e30a86ff04c4a97bfb4d61237d0f6ed
Udit Gupta <udit2008.gupta@gmail.com>
2018-09-09 20:42:58 +0530

    Add files via upload

--- local1.py
+++ local1.py
@@ -1,16 +1,22 @@
-import re
+import string
 import math
+import time
 clss = dict()
 dct = dict()
 w = set()
+alphabets = list(string.ascii_lowercase)
 def train():
-    f = open("/scratch/ds222-2017/assignment-1/DBPedia.verysmall/verysmall_train.txt",'r')
-    lines = f.readlines()
-    for x in lines:
+    f = open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_train.txt",'r')
+    ln = f.readline()
+    for x in ln:
         words = x.split(" ")
         key = words[0]
         key1 = key.split(',')
+        # print(words)
+        # print(key)
+        # print(key1)
         for k in key1:
+            k = k.lower()
             if (k not in clss):
                 clss[k] = 1
                 dct[k] = dict()
@@ -18,53 +24,58 @@ def train():
                 clss[k] = clss[k] + 1
 
         for i in words[1:]:
+            i = i.lower()
             w.add(i)
             for k in key1:
+                k = k.lower()
                 if(i not in dct[k]):
                     dct[k][i] = 1
                 else:
                     dct[k][i] = dct[k][i] + 1
-            
+    
     print(len(w))
     
     
 def test():
-    f = open("/scratch/ds222-2017/assignment-1/DBPedia.verysmall/verysmall_test.txt",'r')
-    lines = f.readlines()
-    total_cnt = 0
+    f = open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_test.txt",'r')
+    ln = f.readline()
+    total_cnt = 0.0
     cnt = 0
-    dd=1
-    smoothing = 1
-    lw = len(w)
-    for x in lines:
-        total_cnt = total_cnt + 1
+    smoothing = 1.0
+    lw = len(w)*1.0
+    for x in ln:
+        total_cnt = total_cnt + 1.0
         words = x.split(" ")
         key = words[0]
         key1 = key.split(',')
         predicted = ""
-        max_class = 0
+        max_class = 0.0
         for c in clss:
-            clp = (clss[c]) / sum(clss.values())
+            clp = (clss[c] + (smoothing / lw)) / sum(clss.values())
             value = math.log10(clp)
             fc = dct[c]
-            s = sum(fc.values())
+            s = sum(fc.values())*1.0
             for i in words[1:]:
                 c = fc.get(i)
                 if c is None:
-                    v = 1;
+                    v = 1.0;
                 else:
                     v = c
-                probw = (v + 1) / (s + lw)
+                probw = (v + 1.0 + (smoothing / lw)) / (s + smoothing)
                 value = value + math.log10(probw)
-            if max_class == 0:
+
+            if max_class == 0.0:
                 max_class = value
             if value > max_class:
                 max_class = value
                 predicted = c
+        
         if predicted in key1:
-            cnt = cnt + 1     
+            cnt = cnt + 1
+            
     print((cnt * 100) / total_cnt)  
-tart = time.time()
+
+start = time.time()
 train()
 test()
 print("Total time takem: %f" %(time.time() - start))
